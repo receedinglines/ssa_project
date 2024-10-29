40c95a41783775d8dc71dae9fc69f272be45b42d
receedinglines <sayan.jain@student.uts.edu.au>
2024-10-29 14:11:20 +1100

    User Registration

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 @login_required(login_url='users:login')
 def user(request):
@@ -26,3 +27,14 @@ def logout_view(request):
     logout(request)
     messages.success(request, "Successfully logged out.")
     return redirect('users:login')
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You can now log in.")
+            return redirect('users:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
