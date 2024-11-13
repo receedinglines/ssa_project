20be9366ca1a4aace3886694f94e29cbd0cd7557
receedinglines <sayan.jain@student.uts.edu.au>
2024-11-13 14:58:53 +1100

    Event Creation

--- users/models.py
+++ users/models.py
@@ -27,7 +27,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    nickname = models.CharField(max_length=30, unique=True, null=False, blank=False)
+    nickname = models.CharField(max_length=30, unique=True, null=False, blank=False)    
+    max_spend = models.DecimalField(max_digits=10, decimal_places=2, default=100.00)  # Max spend for each event
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=100.00)  # User's current balance
 
     def clean(self):
         validate_unique_nickname(self.nickname, instance=self)
